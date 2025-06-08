cfc46c2ae57b17438f813d5216cc4321a3817be8
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-08 10:31:56 +0000

    Message to be passed to greeting

--- first-ng-app/src/app/home/home.ts
+++ first-ng-app/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Greeting } from '../components/greeting/greeting';
 
 @Component({
@@ -6,10 +6,11 @@ import { Greeting } from '../components/greeting/greeting';
   imports: [Greeting],
   template: `
     <p>home works!</p>
-    <app-greeting></app-greeting>
+    <app-greeting [message] = "homeMessage()"></app-greeting>
   `,
   styles: ``
 })
 export class Home {
+	homeMessage = signal('Hello World!')
 
 }
