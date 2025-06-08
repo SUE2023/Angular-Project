847923c897691faeb6f3ca40e3403e85b60dd6c6
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-08 13:50:09 +0000

    Counter Component: Imported

--- first-ng-app/src/app/home/home.ts
+++ first-ng-app/src/app/home/home.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { Greeting } from '../components/greeting/greeting';
+import { Counter } from '../components/counter/countrer';
 
 @Component({
   selector: 'app-home',
-  imports: [Greeting],
+  imports: [Greeting, Counter],
   template: `
     <p>home works!</p>
     <app-greeting [message] = "homeMessage()"></app-greeting>
-    <input type = "text" (keyup) = "keyUpHandler($event)">
+    <app-counter></app-counter>
+    <input placeholder = "Type something..." type = "text" (keyup) = "keyUpHandler($event)">
   `,
   styles: ``
 })
