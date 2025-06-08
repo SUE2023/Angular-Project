71e266f49d0fd8cdd3ae340747895752c131414e
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-08 14:59:09 +0000

    Stying: input

--- first-ng-app/src/app/home/home.ts
+++ first-ng-app/src/app/home/home.ts
@@ -1,17 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { Greeting } from '../components/greeting/greeting';
-import { Counter } from '../components/counter/countrer';
+import { Counter } from '../components/counter/counter';
 
 @Component({
   selector: 'app-home',
   imports: [Greeting, Counter],
   template: `
-    <p>home works!</p>
     <app-greeting [message] = "homeMessage()"></app-greeting>
     <app-counter></app-counter>
     <input placeholder = "Type something..." type = "text" (keyup) = "keyUpHandler($event)">
   `,
-  styles: ``
+  styles: [`
+	  input{
+		  margin-top: 10px;
+	  
+	  }`]
 })
 export class Home {
 	homeMessage = signal('Hello World!');
