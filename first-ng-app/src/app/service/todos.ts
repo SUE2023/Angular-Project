d67f365ed7820b552aa00c09dc3fb81eac5b635b
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-08 18:45:00 +0000

    Todo list: Array

---
+++ first-ng-app/src/app/service/todos.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Todos {
+	todoItems: Array<Todo> = [
+		{
+			title: 'groceries',
+			id: 0,
+			userId: 1,
+			completed: false;
+		},
+		{
+			title: 'jobsearch',
+			id: 1;
+			userId: 1,
+			completed: false,
+		},
+		{
+			title: 'learning',
+			id: 2,
+			userId: 1,
+			completed: false,
+		}.
+	];
+
+  constructor() { }
+}
