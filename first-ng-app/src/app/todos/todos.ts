f286bc4679949120ebc931b2ab740dfc07da38ec
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-09 08:21:52 +0000

    Injecting todoService: Todo Component

---
+++ first-ng-app/src/app/todos/todos.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { TodoService } from '../services/todo';
+
+@Component({
+  selector: 'app-todos',
+  imports: [],
+  template: `
+      @for(todo for todoItems(); track.id){
+	      <p>{{todo.title}}</p>
+      }
+    
+  `,
+  styles: ``
+})
+export class Todos implements OnInit{
+	todoService = inject(TodoService);
+	todoItems = signal <Array<Todo>>([]);
+
+	ngOnInit(): void{
+		console.log(this.todoService.todoItems);
+		this.todoItems.set(this.todService.todItems);
+	}
+
+}
